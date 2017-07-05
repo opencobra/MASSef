0e9eddcd05f35b40d4460bfd01f62c332ecfe55e
martamatos <marta.ra.matos@gmail.com>
2017-07-05 13:55:30 +0200

    changes due to dropbox synchronization

--- python_code/src/run_pso.py
+++ python_code/src/run_pso.py
@@ -1,8 +1,8 @@
 from pso_short_ver0091_rel_marta import run_pso
 import sys
 
-def _parse_function_parameters(parameter_file_in):
 
+def _parse_function_parameters(parameter_file_in):
     """
     These parameters are imported from a text file constructed using the accompanying mathematica notebook.
     The parser expects two tab delimited elements in each line, which are placed in a dictionary object with
@@ -38,7 +38,6 @@ def _parse_function_parameters(parameter_file_in):
 
 
 def test_run_fit():
-
     pso_parameter_file_in = '/home/mrama/Dropbox/Kinetics/Enzymes_new/G6PDH2r_dKd/Parameter_Fitting/Development/psoParameters.txt'
     lma_parameter_file_in = '/home/mrama/Dropbox/Kinetics/Enzymes_new/G6PDH2r_dKd/Parameter_Fitting/Development/lmlaParameters.txt'
 
@@ -47,18 +46,19 @@ def test_run_fit():
     pso_ultimate_result_file_name = '/home/mrama/Dropbox/Kinetics/Enzymes_new/G6PDH2r_dKd/Parameter_Fitting/Development/psoResults.txt'
     lma_candidates_import_path = pso_ultimate_result_file_name
     lma_candidates_export_path = '/home/mrama/Dropbox/Kinetics/Enzymes_new/G6PDH2r_dKd/Parameter_Fitting/Development/lmaResults.txt'
-    pso_num_trials = 1        # Final candidate values
-    data_file_name = '/home/mrama/Dropbox/Kinetics/Enzymes_new/G6PDH2r_dKd/Parameter_Fitting/Development/G6PDH2r.dat'      # path to file with the data to be fit
+    pso_num_trials = 1  # Final candidate values
+    data_file_name = '/home/mrama/Dropbox/Kinetics/Enzymes_new/G6PDH2r_dKd/Parameter_Fitting/Development/G6PDH2r.dat'  # path to file with the data to be fit
 
     # run pso
     pso_parameters = _parse_function_parameters(pso_parameter_file_in)
-    pso_num_trials=1
+    pso_num_trials = 1
     for i in range(pso_num_trials):
         run_pso(pso_parameters, data_file_name, pso_summary_file_name, pso_ultimate_result_file_name)
 
-    # run lma
-    #lma_parameters = _parse_function_parameters(lma_parameter_file_in)
-    #run_lma(lma_parameters, data_file_name, lma_candidates_import_path, lma_candidates_export_path)
+        # run lma
+        # lma_parameters = _parse_function_parameters(lma_parameter_file_in)
+        # run_lma(lma_parameters, data_file_name, lma_candidates_import_path, lma_candidates_export_path)
+
 
 if __name__ == '__main__':
 
@@ -66,12 +66,12 @@ if __name__ == '__main__':
     lma_parameter_file_in = sys.argv[2]  # parameter file for lma
 
     ## File Pathways for Recording Information
-    pso_summary_file_name = sys.argv[3]   # summary of each pso trial
+    pso_summary_file_name = sys.argv[3]  # summary of each pso trial
     pso_ultimate_result_file_name = sys.argv[4]  # final pso candidate values, same as initial candidates for lma
     lma_candidates_import_path = pso_ultimate_result_file_name
     lma_candidates_export_path = sys.argv[5]
-    pso_num_trials = int(sys.argv[6])          # Final candidate values
-    data_file_name = sys.argv[7]        # path to file with the data to be fit
+    pso_num_trials = int(sys.argv[6])  # Final candidate values
+    data_file_name = sys.argv[7]  # path to file with the data to be fit
 
     # run pso
     pso_parameters = _parse_function_parameters(pso_parameter_file_in)
