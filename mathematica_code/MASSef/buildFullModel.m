12b9dac3931c4ab9be754913f77b314a74dc9cb2
martamatos <marta.ra.matos@gmail.com>
2018-07-15 17:50:49 +0200

    Updated buildFullModel

--- mathematica_code/MASSef/buildFullModel.m
+++ mathematica_code/MASSef/buildFullModel.m
@@ -16,10 +16,10 @@ Begin["`Private`"];
 
 
 buildFullEnzymeModel[enzymeModel_, rxn_, pathMASSef_, inputPath_, outputPath_, dataFileName_, inhibitionList_,inhibitionList_, KeqList_, 
-					 kmList_, s05List_, kcatList_, inhibitionList_, activationList_, otherParmsList_,inhibitionListSubset_,bufferInfo_, ionCharge_,
+					 kmList_, s05List_, kcatList_, inhibitionList_, activationList_, otherParmsList_, inhibitionListSubset_,bufferInfo_, ionCharge_,
 					 catalyticReactionsSetsList_, otherMetsReverseZeroSub_,  otherMetsForwardZeroSub_,   customRatiosDataList_, MWCFlag_,
 					 simplifyFlag_, simplifyMaxTime_, nActiveSites_, fitLabel_, numTrials_, simulateDataFlag_, nSamples_, paramScanList_, 
-					 flagFitType_:"abs_ssd"]:=
+					 assumedSaturatingConc_:1, flagFitType_:"abs_ssd", equivalentReactionsSetsList_:{}]:=
 
 	Block[{ haldaneRatiosList,  metSatForSub, metSatRevSub,  finalRateConsts, metsFull, metsSub, rateConstsSub, 
 			fileList, fileListSub, eqnNameList,eqnValList, eqnValListPy, 
@@ -36,32 +36,35 @@ buildFullEnzymeModel[enzymeModel_, rxn_, pathMASSef_, inputPath_, outputPath_, d
 	allCatalyticReactions,nonCatalyticReactions, unifiedRateConstList, 
 	absoluteFlux, absoluteRateForward, absoluteRateReverse, relativeRateForward, relativeRateReverse, 
 	otherAbsoluteRatesForward, otherAbsoluteRatesReverse}=
-		setUpRateEquations[enzymeModel, rxn, getID@rxn, inputPath, inhibitionList,inhibitionListSubset, catalyticReactionsSetsList,   
-		  				 otherMetsReverseZeroSub, otherMetsForwardZeroSub,  MWCFlag, simplifyFlag, simplifyMaxTime, nActiveSites];
+		setUpRateEquations[enzymeModel, rxn, getID@rxn, inputPath, inhibitionList, inhibitionListSubset, catalyticReactionsSetsList,   
+		  				 otherMetsReverseZeroSub, otherMetsForwardZeroSub,  MWCFlag, simplifyFlag, simplifyMaxTime, nActiveSites,
+		  				 assumedSaturatingConc, equivalentReactionsSetsList];
 
 	Which[StringMatchQ[StringTrim[ToLowerCase@simulateDataFlag], "normal"],
 
-		{allFittingData, dataPathList, fileList, fileListSub} = simulateData[enzymeModel,dataFileName, haldaneRatiosList, KeqList, kmList, s05List, 
-				kcatList, inhibitionList, activationList, otherParmsList, rxn, metsFull,  
+		{allFittingData, dataPathList, fileList, fileListSub} = simulateData[enzymeModel,dataFileName, fitLabel, haldaneRatiosList, KeqList, 
+				kmList, s05List, kcatList, inhibitionList, activationList, otherParmsList, rxn, metsFull,  
 				metSatForSub, metSatRevSub,  bufferInfo, ionCharge, inputPath,  fileList, fileListSub, 
-				eqnNameList,eqnValList, eqnValListPy, eqnNameList, rateConstsSub, 
-				metsSub, allCatalyticReactions,nonCatalyticReactions, unifiedRateConstList, customRatiosDataList];,
+				eqnNameList, eqnValList, eqnValListPy, eqnNameList, rateConstsSub, 
+				metsSub, allCatalyticReactions ,nonCatalyticReactions, unifiedRateConstList, customRatiosDataList,
+				assumedSaturatingConc];,
+				
 
 		StringMatchQ[StringTrim[ToLowerCase@simulateDataFlag], "uncertainty"],
 		
-		{allFittingData, dataPathList, fileList, fileListSub} = simulateDataWithUncertainty[nSamples,enzymeModel,dataFileName, haldaneRatiosList, KeqList, 
+		{allFittingData, dataPathList, fileList, fileListSub} = simulateDataWithUncertainty[nSamples,enzymeModel,dataFileName, fitLabel, haldaneRatiosList, KeqList, 
 				kmList, s05List, kcatList, inhibitionList, activationList, otherParmsList, rxn, metsFull,  metSatForSub, metSatRevSub, otherParmsList,  
 				bufferInfo, ionCharge, inputPath,  fileList, fileListSub, eqnNameList,eqnValList, eqnValListPy, eqnNameList, rateConstsSub, 
-				metsSub, allCatalyticReactions,nonCatalyticReactions, unifiedRateConstList,  customRatiosDataList];,
+				metsSub, allCatalyticReactions,nonCatalyticReactions, unifiedRateConstList,  customRatiosDataList, assumedSaturatingConc];,
 
 		StringMatchQ[StringTrim[ToLowerCase@simulateDataFlag], "param_scan"],
 		
 		{allFittingData, dataPathList, fileList, fileListSub} = simulateParameterScanData[paramScanList, enzymeModel, dataFileName, 
-			  haldaneRatiosList, KeqList, kmList, s05List, kcatList, inhibitionList, activationList, 
+			  fitLabel, haldaneRatiosList, KeqList, kmList, s05List, kcatList, inhibitionList, activationList, 
 			  otherParmsList, rxn, metsFull, metSatForSub, metSatRevSub,  bufferInfo, 
 			 ionCharge, inputPath, fileList, fileListSub, eqnNameList, 
 			 eqnValList, eqnValListPy, eqnNameList, rateConstsSub, metsSub, allCatalyticReactions,
-			 nonCatalyticReactions, unifiedRateConstList, customRatiosDataList];
+			 nonCatalyticReactions, unifiedRateConstList, customRatiosDataList, assumedSaturatingConc];
 	];
 	
 	Print["Configuring enzyme fits..."];
@@ -72,14 +75,15 @@ buildFullEnzymeModel[enzymeModel_, rxn_, pathMASSef_, inputPath_, outputPath_, d
 	{lmaParameterPath, lmaResultsFile} = defineLMAparameters[inputPath, outputPath, finalRateConsts, fileList, lowerParamBound, upperParamBound, fitLabel];
 
 	Print["Running enzyme fit..."];
+	Print[numTrials];
 	runFit[inputPath, pathMASSef, psoParameterPath ,lmaParameterPath,psoTrialSummaryFileName, 
 			psoResultsFile, lmaResultsFile, numTrials, dataPathList];
 	
 	Print["Evaluating fitness results..."];
-	{flagFitLocal, msgLocal, fittingData, filteredDataList, bestFitDetails}=getRatesWithSSD[rxnName, lmaResultsFile, dataPathList, inputPath, outputPath,  fileListSub, 
-				rateConstsSub, metsSub, flagFitType, True, fitLabel, Null];
-	Print["ENd"];
-	Return[{fittingData, filteredDataList, bestFitDetails,
+	{flagFitLocal, msgLocal, fittingData, filteredDataList, bestFitDetails}=getRatesWithSSD[getID@rxn, lmaResultsFile, dataPathList, inputPath, outputPath,  fileListSub, 
+				rateConstsSub, metsSub, flagFitType, Null, True, fitLabel<>"_"<>flagFitType];
+	Print["End"];
+	Return[{fittingData, filteredDataList, bestFitDetails, dataPathList,
 			{enzymeModelLocal, haldaneRatiosList,  metSatForSub, metSatRevSub, rateConstsSub,
 			allCatalyticReactions,nonCatalyticReactions, absoluteFlux, absoluteRateForward,
 			absoluteRateReverse, relativeRateForward, relativeRateReverse, 
