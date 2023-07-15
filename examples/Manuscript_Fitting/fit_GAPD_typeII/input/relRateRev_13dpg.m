224635c2d28d1eda2e98c38bb62ddfb75c7adc2c
Dan Zielinski <dczielin@ucsd.edu>
2023-07-15 11:01:47 -0700

    updated GAPD standardization

--- examples/Manuscript_Fitting/fit_GAPD_typeII/input/relRateRev_13dpg.m
+++ examples/Manuscript_Fitting/fit_GAPD_typeII/input/relRateRev_13dpg.m
@@ -1,19 +1,21 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
 (metabolite["13dpg", "c"]*(rateconst["GAPD1", False]*
-    (metabolite["nadh", "c"]*rateconst["GAPD2", False] + 
-     rateconst["GAPD2", True])*rateconst["GAPD3", False]*
-    (rateconst["GAPD5", False]*rateconst["GAPD6", False] + 
-     rateconst["GAPD4", True]*(rateconst["GAPD5", False] + 
-       rateconst["GAPD6", True])) + rateconst["GAPD4", False]*
-    (rateconst["GAPD1", False]*rateconst["GAPD3", False]*
+    rateconst["GAPD3", False]*(rateconst["GAPD2", True]*
       rateconst["GAPD5", False]*rateconst["GAPD6", False] + 
-     metabolite["nadh", "c"]*rateconst["GAPD2", False]*
-      (rateconst["GAPD1", False]*rateconst["GAPD5", False]*
-        rateconst["GAPD6", False] + rateconst["GAPD3", False]*
-        rateconst["GAPD5", False]*rateconst["GAPD6", False] + 
-       rateconst["GAPD1", False]*rateconst["GAPD3", False]*
-        (rateconst["GAPD5", False] + rateconst["GAPD6", False] + 
-         rateconst["GAPD6", True])))))/
+     rateconst["GAPD4", False]*rateconst["GAPD5", False]*
+      rateconst["GAPD6", False] + rateconst["GAPD2", True]*
+      rateconst["GAPD4", True]*(rateconst["GAPD5", False] + 
+       rateconst["GAPD6", True])) + metabolite["nadh", "c"]*
+    rateconst["GAPD2", False]*(rateconst["GAPD1", False]*
+      rateconst["GAPD4", False]*rateconst["GAPD5", False]*
+      rateconst["GAPD6", False] + rateconst["GAPD3", False]*
+      rateconst["GAPD4", False]*rateconst["GAPD5", False]*
+      rateconst["GAPD6", False] + rateconst["GAPD1", False]*
+      rateconst["GAPD3", False]*(rateconst["GAPD5", False]*
+        rateconst["GAPD6", False] + rateconst["GAPD4", True]*
+        (rateconst["GAPD5", False] + rateconst["GAPD6", True]) + 
+       rateconst["GAPD4", False]*(rateconst["GAPD5", False] + 
+         rateconst["GAPD6", False] + rateconst["GAPD6", True])))))/
  (rateconst["GAPD1", False]*(metabolite["nadh", "c"]*
      rateconst["GAPD2", False] + rateconst["GAPD2", True])*
    rateconst["GAPD3", False]*(rateconst["GAPD5", False]*
